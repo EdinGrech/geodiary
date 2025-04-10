b1d9a158f7c923998865cc74cafda78ddc105a6b
eding <edin.grech@gmail.com>
2025-04-10 21:01:34 +0200

    feat: added location service

---
+++ lib/services/location_service.dart
@@ -0,0 +1,26 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:geodiary/utils/permissions.dart';
+
+class LocationService {
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    LocationPermission permission = await Permissions.checkAndRequestLocationPermission();
+
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.medium,
+    );
+  }
+}
