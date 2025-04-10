788184d67bcd0c1fa00b93ab9a48c5c75ad0dbce
eding <edin.grech@gmail.com>
2025-04-10 20:57:08 +0200

    chore: main screen init

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:geodiary/services/firebase_service.dart';
+import 'package:geodiary/widgets/entry_list_item.dart';
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GeoDiary Entries'),
+      ),
+    );
+  }
+}
